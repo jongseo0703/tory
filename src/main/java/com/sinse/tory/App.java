544845ee73d779de62f72a78bf5509f5897924f9
jongseo0703 <jongseo0703@naver.com>
2025-06-12 23:31:19 +0900

    tory프로젝트생성

---
+++ src/main/java/com/sinse/tory/App.java
@@ -0,0 +1,13 @@
+package com.sinse.tory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
