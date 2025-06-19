f14307aaac8b6ba858a45e022b6845f81b177f34
DESKTOP-2HSGONB\a <sohee00314@gmail.com>
2025-06-19 17:47:45 +0900

    ✨ Feat : 페이지기 기본 크기 설정이 들어있음

---
+++ src/main/java/com/sinse/tory/db/common/util/PageUtil.java
@@ -0,0 +1,12 @@
+package com.sinse.tory.db.common.util;
+
+import javax.swing.JPanel;
+
+public class PageUtil extends JPanel{
+	
+	public static int Tory_Width = 1440;
+	public static int Tory_Hieght =820;
+	public static int InputOutput_Width = Tory_Width/2;
+	
+	
+}
