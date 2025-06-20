a86c00d3bf82f9f9fb50ccc966634b8490edbfb0
DESKTOP-2HSGONB\a <sohee00314@gmail.com>
2025-06-20 17:07:06 +0900

    추가

--- src/main/java/com/sinse/tory/db/common/util/PageUtil.java
+++ src/main/java/com/sinse/tory/db/common/util/PageUtil.java
@@ -8,5 +8,9 @@ public class PageUtil extends JPanel{
 	public static int Tory_Hieght =820;
 	public static int InputOutput_Width = Tory_Width/2;
 	
+	//페이지 설정
+	public static int ProductshipPage = 0;
+	public static int Page = 1;
+	
 	
 }
