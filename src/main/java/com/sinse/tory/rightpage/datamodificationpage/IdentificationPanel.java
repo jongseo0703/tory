239a7e31c479acdbc5f61fb740615920f5c04ff7
dgw0103 <dgw0103@naver.com>
2025-06-23 02:38:42 +0900

    ✨ Feat : 콤보박스 변경에 대한 이벤트 제작 중

--- src/main/java/com/sinse/tory/rightpage/datamodificationpage/IdentificationPanel.java
+++ src/main/java/com/sinse/tory/rightpage/datamodificationpage/IdentificationPanel.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.util.EventListener;
 import java.util.List;
 
 import javax.swing.Box;
@@ -13,7 +14,9 @@ import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import com.sinse.tory.db.model.SubCategory;
 import com.sinse.tory.db.model.TopCategory;
+import com.sinse.tory.db.repository.SubCategoryDAO;
 import com.sinse.tory.db.repository.TopCategoryDAO;
 
 final class IdentificationPanel extends JPanel
@@ -24,13 +27,25 @@ final class IdentificationPanel extends JPanel
 	private LabelComponentSet name;
 	//
 	
+	private TopCategory selectedTopCategory;
+	private SubCategory selectedSubCategory;
+	
+	private JComboBox<TopCategory> topCategoryComboBox;
+	private JComboBox<SubCategory> subCategoryComboBox;
+	
 	
 	
 	IdentificationPanel()
 	{
-		topCategory = new LabelComponentSet("상위 카테고리", getTopCategoryComboBox());
-		subCategory = new LabelComponentSet("하위 카테고리", new JComboBox<String>());
-		name = new LabelComponentSet("상품 명", new JTextField());
+		topCategoryComboBox = new JComboBox<TopCategory>();
+		subCategoryComboBox = new JComboBox<SubCategory>();
+		
+		topCategory = new LabelComponentSet("상위 카테고리", topCategoryComboBox, true);
+		subCategory = new LabelComponentSet("하위 카테고리", new JComboBox<String>(), true);
+		name = new LabelComponentSet("상품 명", new JTextField(), false);
+		
+		selectedTopCategory = null;
+		selectedSubCategory = null;
 		
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		add(topCategory);
@@ -42,22 +57,47 @@ final class IdentificationPanel extends JPanel
 	
 	
 	
-	private JComboBox<TopCategory> getTopCategoryComboBox()
+	private void updateTopCategoryComboBox()
 	{
-		JComboBox<TopCategory> comboBox = new JComboBox<TopCategory>();
 		TopCategoryDAO topCategoryDAO = new TopCategoryDAO();
 		List<TopCategory> topCategorys = topCategoryDAO.selectAll();
 		
+		// 다른 컴포넌트들 비활성화, 활성화
+		topCategoryComboBox.addItemListener(new ComboBoxSelectionAction(subCategoryComboBox));
+		
+		// combo box item 초기화
 		TopCategory dummy = new TopCategory();
 		dummy.setTopCategoryId(0);
 		dummy.setTopCategoryName("상위 카테고리를 선택 하세요.");
 		
-		comboBox.addItem(dummy);
+		topCategoryComboBox.addItem(dummy);
 		for(int i = 0; i < topCategorys.size(); i++)
 		{
-			comboBox.addItem(topCategorys.get(i));
+			topCategoryComboBox.addItem(topCategorys.get(i));
+		}
+		//
+	}
+	// TODO : top category 선택할 때마다 최신화
+	private JComboBox<SubCategory> getSubCategoryComboBox()
+	{
+		JComboBox<SubCategory> comboBox = new JComboBox<SubCategory>();
+		SubCategoryDAO subCategoryDAO = new SubCategoryDAO();
+		List<SubCategory> subCategorys = subCategoryDAO.selectByTop(selectedTopCategory);
+		
+		SubCategory dummy = new SubCategory();
+		dummy.setSubCategoryId(0);
+		dummy.setSubCategoryName("상위 카테고리를 선택 하세요.");
+		
+		comboBox.addItem(dummy);
+		for(int i = 0; i < subCategorys.size(); i++)
+		{
+			comboBox.addItem(subCategorys.get(i));
 		}
 		
 		return comboBox;
+	}
+	private void updateIdentifierState()
+	{
+		
 	}
 }
--- src/main/java/com/sinse/tory/rightpage/datamodificationpage/LabelComponentSet.java
+++ src/main/java/com/sinse/tory/rightpage/datamodificationpage/LabelComponentSet.java
@@ -20,20 +20,22 @@ public final class LabelComponentSet extends JPanel
 	
 	
 	
-	public LabelComponentSet(String labelText, JComponent component)
+	public LabelComponentSet(String labelText, JComponent component, boolean enabled)
 	{
 		label = new JLabel();
 		this.component = component;
 		
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
 		add(label, BorderLayout.NORTH);
 		add(Box.createRigidArea(new Dimension(0, 8)));
 		add(component, BorderLayout.SOUTH);
 		
 		label.setText(labelText);
 		label.setAlignmentX(LEFT_ALIGNMENT);
-		component.setAlignmentX(LEFT_ALIGNMENT);
 		
+		component.setAlignmentX(LEFT_ALIGNMENT);
+		component.setEnabled(enabled);
 		component.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
 	}
 }
