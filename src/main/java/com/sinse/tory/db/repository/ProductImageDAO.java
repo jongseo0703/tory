3d65055f6c9a0efafdec6792c3c1c76ec0210615
heeezni <hjin3456@naver.com>
2025-06-26 06:27:25 +0900

    ✨ Feat: ProductImageDAO 구현

--- src/main/java/com/sinse/tory/db/repository/ProductImageDAO.java
+++ src/main/java/com/sinse/tory/db/repository/ProductImageDAO.java
@@ -1,5 +1,37 @@
 package com.sinse.tory.db.repository;
 
-public class ProductImageDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import com.sinse.tory.db.common.util.DBManager;
+
+public class ProductImageDAO {
+    // 싱글톤 패턴인 DBManager 클래스를 통해 하나의 인스턴스인 dbManger생성
+    DBManager dbManager = DBManager.getInstance();
+
+    /**
+     * 이미지 경로 DB에 저장
+     */
+    public void insertImagePath(String imagePath, int productId) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        con = dbManager.getConnection();
+        StringBuffer sql = new StringBuffer();
+        sql.append("INSERT INTO ProductImage (image_url, product_id) VALUES (?, ?)");
+
+        try {
+            pstmt = con.prepareStatement(sql.toString());
+            pstmt.setString(1, imagePath);
+            pstmt.setInt(2, productId);
+            pstmt.executeUpdate();
+            pstmt.executeUpdate();
+            System.out.println("✅ 이미지 경로 저장 완료: " + imagePath);
+        } catch (SQLException e) {
+            System.out.println("이미지 경로 저장 실패");
+            e.printStackTrace();
+        }
+
+    }
 }
