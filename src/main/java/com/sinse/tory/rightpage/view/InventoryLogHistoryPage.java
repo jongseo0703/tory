c4d6e735c6dd75ecca1b5f8f51445687e9a37a7e
jongseo0703 <jongseo0703@naver.com>
2025-06-26 18:35:18 +0900

    ♻️ Refactor : 입출고 내역 다른 페이지 갔다오면 초기화 하는 기능 수정

--- src/main/java/com/sinse/tory/rightpage/view/InventoryLogHistoryPage.java
+++ src/main/java/com/sinse/tory/rightpage/view/InventoryLogHistoryPage.java
@@ -3,6 +3,8 @@ package com.sinse.tory.rightpage.view;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Locale;
@@ -58,6 +60,14 @@ public class InventoryLogHistoryPage extends Pages {
         setupLayout();
         setupEventListeners();
         styleComponents();
+        
+        // 다른 페이지 갔다오면 초기화 하기
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentShown(ComponentEvent e) {
+                resetFilters();
+            }
+        });
         
         // 페이지 로드 시 전체 입출고 내역 자동 로드
         loadAllInventoryHistory();
